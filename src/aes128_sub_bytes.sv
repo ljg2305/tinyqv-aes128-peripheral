272ecfb23f4044b45d49af18143aa718d911e347
Lawrence Gray <ljg2305@gmail.com>
2025-08-01 10:49:37 +0100

    parameterise sub_bytes for reuse in keygen

--- src/aes128_sub_bytes.sv
+++ src/aes128_sub_bytes.sv
@@ -1,12 +1,18 @@
-module aes128_sub_bytes (
-    input logic         clk_i, 
-    input logic         rst_n_i,
-    input logic [127:0] data_i, 
-    input logic         start_i, 
-    output logic [7:0]  data_o, 
-    output logic [3:0]  addr_o, 
-    output logic        valid_o,
-    output logic        done_o
+module aes128_sub_bytes #(
+    int N_BYTES = 16,
+    logic EXTERNAL_SBOX = 0
+    ) (
+    input logic                  clk_i, 
+    input logic                  rst_n_i,
+    input logic [N_BYTES*8 -1:0] data_i, 
+    input logic                  start_i, 
+    output logic [7:0]           data_o, 
+    output logic [3:0]           addr_o, 
+    output logic                 valid_o,
+    output logic                 done_o,
+    // external S-BOX signals 
+    output logic [7:0]           sbox_sub_o, 
+    input logic  [7:0]           sbox_sub_i
     );
 
     // Rijndael S-Box LUT signals
@@ -34,7 +40,7 @@ module aes128_sub_bytes (
         case (state) 
             WAIT:   if (start_i) next_state = OUTPUT;
             OUTPUT: begin 
-                if (byte_count >= 15) next_state = WAIT; 
+                if (byte_count >= N_BYTES-1) next_state = WAIT; 
                 sbox_data_in  = data_i[byte_count*8+:8];
                 data_o = sbox_data_out;
                 valid_o = 1'b1; 
@@ -53,11 +59,19 @@ module aes128_sub_bytes (
         end
     end
 
-    // Rijndael S-Box LUT signals
-    aes128_rijndael_sbox aes128_rijndael_sbox_inst (
-        .data_i(sbox_data_in),
-        .data_o(sbox_data_out)
-    );
+     generate
+        if (EXTERNAL_SBOX) begin 
+            assign sbox_sub_o = sbox_data_in;
+            assign sbox_data_out = sbox_sub_i; 
+        end else begin 
+            assign sbox_sub_o = '0; 
+            // Rijndael S-Box LUT signals
+            aes128_rijndael_sbox aes128_rijndael_sbox_inst (
+                .data_i(sbox_data_in),
+                .data_o(sbox_data_out)
+            );
+        end 
+    endgenerate
     
     // DONE SIGNAL 
     assign done_o = (next_state==WAIT && state != WAIT) ? 1'b1 : 1'b0;
